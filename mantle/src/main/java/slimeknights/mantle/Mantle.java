3f5c74050e60aa185e87427baaad42cd930384ce
Reinier The Ghost <reinierrolbrouwer@gmail.com>
2026-08-19 17:54:18 +0200

    help

---
+++ mantle/src/main/java/slimeknights/mantle/Mantle.java
@@ -0,0 +1,12 @@
+package slimeknights.mantle;
+
+import net.neoforged.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Mantle.modId)
+public class Mantle {
+    public static final String modId = "mantle";
+    public static final Logger logger = LogManager.getLogger("Mantle");
+    /** Namespace for common tags, used for easier migration to the future "c" standard */
+}
